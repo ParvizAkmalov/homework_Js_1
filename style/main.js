f484775d73b0ffeddf516e1a1137397203c1d945
Parviz_8901 <parvizakmalov39@gmail.com>
2024-02-21 23:20:35 +0500

    Js_homework_1

---
+++ style/main.js
@@ -0,0 +1,35 @@
+// js 1
+var admin = namer
+var namer = "Джон"
+
+admin = namer
+
+console.log(admin);
+console.log(namer);
+
+
+// js 2
+
+//Баланс ползовитела
+let balance = 25000
+console.log(balance);
+
+// Траты
+let car = 4480
+let food = 890
+
+// Доходы
+let invoice = 5500
+let stock = 4000
+
+// Итоги
+let totalBalance = 0
+
+
+// bitta bitta
+console.log(balance - car);
+console.log(balance - car - food);
+console.log(balance - car - food + invoice);
+
+// TotalBalance
+console.log(balance - car - food + invoice + stock);
